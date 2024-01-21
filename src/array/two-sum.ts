d91744cb0f82cf6f1ef6b32fd9a8fd062859360b
Harshal Nikhare <harshaln369@gmail.com>
2024-01-21 08:26:28 +0530

    two sum prac

--- src/array/two-sum.ts
+++ src/array/two-sum.ts
@@ -1,12 +1,14 @@
+type Matches = { [key: string]: number };
 function twoSum(nums: number[], target: number): number[] | undefined {
-  const matches = {};
+  const matches: Matches = {};
+
   for (let i = 0; i < nums.length; i++) {
-    const number = nums[i];
-
-    if (Number.isInteger(matches[number])) return [matches[number], i];
-
-    if (!matches[number]) {
-      matches[target - number] = i;
+    if (matches[nums[i]] === undefined) {
+      matches[target - nums[i]] = i;
+    } else {
+      return [matches[nums[i]], i];
     }
   }
 }
+
+// console.log(twoSum([2, 7, 11, 15], 9));
